c3b304e4d2f59f5842db89728994d304a24729ba
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2025-08-27 20:40:43 -0400

    05-Final configure send email

--- src/main/java/com/trevisan/EmailSender/Infra/Services/SesService.java
+++ src/main/java/com/trevisan/EmailSender/Infra/Services/SesService.java
@@ -1,6 +1,7 @@
 package com.trevisan.EmailSender.Infra.Services;
 
 import com.trevisan.EmailSender.Adapters.EmailSenderGateway;
+import com.trevisan.EmailSender.Email.EmailTemplates;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.ses.SesClient;
@@ -10,21 +11,22 @@ import software.amazon.awssdk.services.ses.model.*;
 @Service
 public class SesService implements EmailSenderGateway {
     private final SesClient sesClient;
-    private final String bodyHtml = "";
+    private final EmailTemplates templates;
 
     @Autowired
-    public SesService(SesClient sesClient) {
+    public SesService(SesClient sesClient, EmailTemplates templates) {
         this.sesClient = sesClient;
+        this.templates = templates;
     }
 
     @Override
     public String senderEmail(String to, String subject) {
-        send(sesClient, to, subject, bodyHtml);
+        send(sesClient, to, subject);
         sesClient.close();
         return "Email enviado!";
     }
 
-    private static void send(SesClient sesClient, String to, String subject, String messageHtml) {
+    private void send(SesClient sesClient, String to, String subject) {
         Destination destination = Destination.builder()
                 .toAddresses(to)
                 .build();
@@ -34,7 +36,7 @@ public class SesService implements EmailSenderGateway {
                 .build();
 
         Content contentHtml = Content.builder()
-                .data(messageHtml)
+                .data(templates.bodyWithHtmlV1)
                 .build();
 
         Body body = Body.builder()
@@ -44,15 +46,19 @@ public class SesService implements EmailSenderGateway {
 
         Message message = Message.builder()
                 .subject(contentSubject)
+                .body(body)
                 .build();
+
+
+        var emailRequest = this.sendEmailRequest(message, destination, to);
+        sesClient.sendEmail(emailRequest);
     }
 
-    private void sendEmail(Destination destination, Message message) {
-        sesClient.sendEmail(
-                new SendEmailRequest.Builder()
-                .destination(destination)
-                .source()
+    private SendEmailRequest sendEmailRequest(Message message, Destination destination, String to) {
+        return SendEmailRequest.builder()
                 .message(message)
-                .build());
+                .destination(destination)
+                .source(to)
+                .build();
     }
 }
